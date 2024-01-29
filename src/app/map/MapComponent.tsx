4d81a01015f6a6159697be68132ee2a8c8554225
afcirillo96 <agustincirillo@hotmail.com>
2024-01-29 19:26:10 -0300

    controls added & initial zoom & coords fixed

--- src/app/map/MapComponent.tsx
+++ src/app/map/MapComponent.tsx
@@ -1,22 +1,21 @@
 'use client'
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import maplibregl from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css'; // Asegúrate de importar los estilos CSS
 
 const MapComponent = () => {
   useEffect(() => {
     const map = new maplibregl.Map({
       container: 'map',
       style: '/style.json',
-      center: [ -59,-40],
-      zoom: 4,
+      center: [-64.9395,-40.5736],
+      zoom: 3.7,
       bearing: 0,
       pitch: 0,
     });
     
-    // map.on('load', function() {
-    //   // Ocultar el control de atribuciones
-    //   map.getElementsByClassName('maplibregl-control-container')[0].style.display ='none'
-    // });
+    // Agregar controles
+    map.addControl(new maplibregl.NavigationControl(), 'top-right');
     
     return () => map.remove();
   }, []);
