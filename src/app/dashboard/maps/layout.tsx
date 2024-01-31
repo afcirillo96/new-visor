f363fd107db5627f8b730b4c28b5b5ce1e349cea
afcirillo96 <agustincirillo@hotmail.com>
2024-01-31 12:07:42 -0300

    more pages added

---
+++ src/app/dashboard/maps/layout.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+import React, { useState, useEffect } from 'react';
+import { motion, useAnimation } from 'framer-motion';
+import styles from '@/app/components/Sidebar.module.css';
+
+export default function Layout() {
+    return (
+      <div>
+        <motion.div className={styles.sidebarSubmenu}>
+            <div className='grow'>
+                <p>Mapas</p>
+            </div>
+        </motion.div>
+    </div>
+    );
+}
