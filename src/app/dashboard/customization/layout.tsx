fd8414a42b8c3f5c34b4ac9ff939d28169935bef
afcirillo96 <agustincirillo@hotmail.com>
2024-02-05 12:45:30 -0300

    config simple forma added

--- src/app/dashboard/customization/layout.tsx
+++ src/app/dashboard/customization/layout.tsx
@@ -47,18 +47,31 @@ export default function Layout() {
         <div>
             <motion.div className={`${styles.subMenu} ${styles.subMenuConfig}`}>
                 <div className='grow'>
+
                     <form onSubmit={handleSubmit}>
                         <h1>Configuración de Estilos</h1>
-                        <label htmlFor="colorFondo">Color de Fondo:</label>
-                        <input
-                            type="color"
-                            id="colorFondo"
-                            value={colorFondo}
-                            onChange={handleChangeColorFondo}
-                        />
-                        <button type="button" onClick={handleDefaultColorClick}>Color por Defecto</button>
-                        <button type="submit">Guardar</button>
+                        <div className="form-group">
+                            <label htmlFor="colorFondo">Color de fondo de la aplicación:</label>
+                            <input
+                                type="color"
+                                id="colorFondo"
+                                value={colorFondo}
+                                onChange={handleChangeColorFondo}
+                            />
+                            <button type="button" onClick={handleDefaultColorClick}>Color por Defecto</button>
+                        </div>
+
+                        <div className="form-group">
+                            <label htmlFor="textSize">Tamaño del texto:</label>
+                            <input type="number" id="textSize" name="textSize" min="8" max="72" required />
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="textColor">Color del texto:</label>
+                            <input type="color" id="textColor" name="textColor" required />
+                        </div>
+                        <button type="submit">Guardar cambios</button>
                     </form>
+
                 </div>
             </motion.div>
         </div>
